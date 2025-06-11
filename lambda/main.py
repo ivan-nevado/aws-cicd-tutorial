5d562da52c188f975cdae1b471379003c689aea7
Ivan Nevado <dsolutionsb3@renta4.es>
2025-06-11 19:22:31 +0200

    Changes

--- lambda/main.py
+++ lambda/main.py
@@ -1,7 +1,11 @@
 def handler(event, context):
     print("Event received:", event)
+    response_body ={
+        'message': 'Hello, World!',
+        'version': '1.0.0',
+    }
     # Your code here
     return {
         'statusCode': 200,
-        'body': 'Hello, World!'
+        'body': response_body
     }
